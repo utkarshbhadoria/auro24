8f1d375995e067a27588947fe159d4f95e2c7617
arshikuk99 <arshikuk4.1@gmail.com>
2025-03-08 15:06:24 +0530

    sidebar upadte pt2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -42,7 +42,7 @@ function Sidebar() {
                 {(handleClick,open)=>{  //this acts as a child component of SidebarLinkGroup ,(handleClick,open) these are props sent by parent
                   return(
                     <React.Fragment>
-                      <NavLink to='#' className={`flex flex-row pl-4 pt-2 pb-2 pr-2 ${pathname.includes("/device")||open ? 'bg-blue-500 rounded-xl' : ''}`} onClick={(e)=>{ handleClick();}}>
+                      <NavLink to='#' className={`flex flex-row pl-4 pt-2 pb-2 pr-2 ${pathname.includes("/deviceProfile")||pathname.includes("/deviceGroups")||pathname.includes("/deviceAssigned")||pathname.includes("/deviceUnassigned") ? 'bg-blue-500 rounded-xl' : ''}`} onClick={(e)=>{ handleClick();}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-monitor-smartphone"><path d="M18 8V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h8"/><path d="M10 19v-3.96 3.15"/><path d="M7 19h5"/><rect width="6" height="10" x="16" y="12" rx="2"/></svg>                        
                         <span className={`overflow-hidden ${expanded? 'w-full pl-2' :'w-0'}`}>Devices</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`lucide lucide-chevron-down ${open && 'rotate-180'}`}><path d="m6 9 6 6 6-6"/></svg>                      
